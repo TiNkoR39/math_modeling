b91501ac1967a69fe6b73f33cec1795ffeb7f535
TiNkoR39 <66479416+TiNkoR39@users.noreply.github.com>
2020-07-01 16:22:04 +0200

    Add files via upload

---
+++ astro_cost.py
@@ -0,0 +1,20 @@
+
+
+earth_mass = 5.94 * 10**24 #si
+
+gravity_const = 6.672 * 10**(-11) #si
+
+rad_orbit_earth = 149.6 * 10**9 #si
+
+rad_earth = 6378.1 * 10**3 #si
+
+rad_sun = 697000 * 10**3 #si
+
+rad_orbit_moon = 384400 * 10**3 #si
+
+rad_moon = 1738 * 10**3 #si
+
+period_earth = 365.2564 #days
+
+speed_of_light = 2.998 * 10**8
+
